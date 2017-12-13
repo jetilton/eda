27131fa33592a30ec273a8267671935f9ce61cbe
jetilton <jfftilton@gmail.com>
2017-12-13 11:25:54 -0800

    fixed boxplot data outliers:

--- boxplot.py
+++ boxplot.py
@@ -5,6 +5,7 @@ from bokeh.plotting import figure, show
 from math import pi
 import seaborn as sns
 import matplotlib.pyplot as plt
+from plots import colors
 
 def df_freq(series, freq = 'year'):
     """
@@ -61,7 +62,7 @@ def boxplot_data(series):
     """
     d = pd.Series()
     s = series.dropna()
-    s = s[~((s-s.mean()).abs()>3.5*s.std())]
+    #s = s[~((s-s.mean()).abs()>5*s.std())]
     d['q1'] = s.quantile(.25)
     d['q2'] = s.quantile(.5)
     d['q3'] = s.quantile(.75)
@@ -72,11 +73,11 @@ def boxplot_data(series):
     d['upper_outer_fence'] = d['q3'] + 3 * d['iq']
     d['upper_whisker'] = series.mean()+3*series.std()
     d['lower_whisker'] = series.mean()-3*series.std()
-    outliers= list(s[s>d['upper_outer_fence']]) + list(s[s<d['lower_outer_fence']])
-    return(d,outliers)
+    outliers= pd.s[(s>d['upper_outer_fence']) | (s<d['lower_outer_fence'])]
+    return (d, outliers)
     
     
-def bokeh_df_box_data(series, freq = 'year'):
+def ts_box_data(series, freq = 'year'):
     
     freq_dict = {
                 'year':'A',
@@ -84,7 +85,7 @@ def bokeh_df_box_data(series, freq = 'year'):
                 'week': 'W'
                  }
     
-    series = series[~((series-series.mean()).abs()>3.5*series.std())]
+    
     groups = series.groupby(pd.Grouper(freq = freq_dict[freq]))
     df = pd.DataFrame()
     names = []
@@ -97,6 +98,7 @@ def bokeh_df_box_data(series, freq = 'year'):
                      }
         try:
             s = pd.Series(group.values)
+            s = s[~((s-s.mean()).abs()>4*s.std())]
             d,outliers = boxplot_data(s)
             ol_df = pd.DataFrame({'outliers':outliers})
             ol_df['date'] = str(name_dict[freq])
@@ -107,25 +109,33 @@ def bokeh_df_box_data(series, freq = 'year'):
             print(name)
             continue
     df['date'] = names
+    df.set_index('date', inplace = True)
     #df['date'] = pd.to_datetime(df['date'])
     ol = pd.concat(ol)
     #ol['date'] = pd.to_datetime(ol['date'])
     return (df, ol)
 
 
-def bokeh_ts_bx_plt(series, title, freq = 'year'):
-    df,ol_df = series.pipe(bokeh_df_box_data, freq = freq) 
+def bk_bx_plt(data, title='', freq = '', w = 1000, h = 400):
     TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
-    p = figure(x_range=[str(x) for x in df['date']], tools=TOOLS, plot_width=1000, plot_height=480, title = title)
+    if freq:
+        df, outliers = data.pipe(ts_box_data, freq = freq) 
+        p = figure(x_range=[str(x) for x in list(df.index)], tools=TOOLS, plot_width=w, plot_height=h, title = title)
+    else:
+        df, outliers = data.pipe(box_data)
+        p = figure(x_range=list(df.index), tools=TOOLS, plot_width=w, plot_height=h, title = title)
+        
     p.xaxis.major_label_orientation = pi/4
     #whiskers
-    p.segment(df.date, df['upper_whisker'], df.date,df['lower_whisker'], color="black")
+    p.segment(df.index, df['upper_whisker'], df.index,df['lower_whisker'], color="black")
     #box
-    p.vbar(x = df['date'], width = .5, bottom = df['q1'], top = df['q3'], fill_color="#D5E1DD", line_color="black")
+    p.vbar(x = df.index, width = .5, bottom = df['q1'], top = df['q3'], fill_color=colors[0], line_color="black")
     #median
-    p.vbar(x = df['date'], width = .5, bottom = df['q2']-.01, top = df['q2']+.01, fill_color="black", line_color="black")
+    p.vbar(x = df.index, width = .5, bottom = df['q2']-.01, top = df['q2']+.01, fill_color="black", line_color="black")
     #outliers
-    p.circle(x = ol_df['date'], y = ol_df['outliers'], size=5, color="#F2583E", alpha=0.5)
+    if outliers.values:
+        
+        p.circle(x = [str(x) for x in outliers.index], y = list(outliers.values), size=10, color=colors[1])
     
     p.xgrid.visible = False
     p.ygrid.visible = False
@@ -135,18 +145,32 @@ def bokeh_ts_bx_plt(series, title, freq = 'year'):
     return p
 
 
-def simple_boxplot(df, title):
-    meta =df.__dict__['metadata']
-    plot_dict = {}
-    for k,v in meta.items():
+def box_data(df):
+    melted = df.melt()
+    groups = melted.groupby(by = 'variable')
+    
+    df = pd.DataFrame()
+    names = []
+    ol = []
+    for name, group in groups:
+        
+        s = pd.Series(pd.DataFrame(group.values).iloc[:,1])
+        
+        s = s[~((s-s.mean()).abs()>5*s.std())]
         try:
-            plot_dict[v['units']].append((k, v['path']))
+            
+            d,outliers = boxplot_data(s)
+            ol_df = pd.DataFrame({'outliers':outliers})
+            ol_df['variable'] = name
+            ol.append(ol_df)
+            df = df.append(d, ignore_index = True)
+            names.append(name)
         except:
-            plot_dict.update({v['units']:[(k, v['path'])]})
-    for k,v in plot_dict.items():
-        data = [df[x[0]].dropna() for x in v]
-        names = [x[1] for x in v]
-        names = [' '.join(x.split('.')[0:2]) for x in names]
-        f, ax = plt.subplots()
-        ax.set_ylabel(k)
-        sns.boxplot(data = data).set(xticklabels=names, title = title)
+            print(name)
+            continue
+    df['variable'] = names
+    df.set_index('variable', inplace = True)
+    #df['date'] = pd.to_datetime(df['date'])
+    ol = pd.concat(ol)
+    #ol['date'] = pd.to_datetime(ol['date'])
+    return (df, ol)
