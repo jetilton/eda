02345efa56932ead75f993d6a0edcf2cff44c8c8
jetilton <jfftilton@gmail.com>
2018-03-21 12:25:34 -0700

    added **kwargs to bk_line

--- eda/plots.py
+++ eda/plots.py
@@ -110,8 +110,8 @@ def bk_autocor(series):
     p.outline_line_alpha = 0.0
     return p
 
-def bk_line(x,y, w = 1000, h = 300, title = '', x_axis_type = 'datetime' ):
-    p = figure(plot_width=w, plot_height=h, x_axis_type = x_axis_type, title = title)
+def bk_line(x,y, w = 1000, h = 300, title = '', x_axis_type = 'datetime', **kwargs ):
+    p = figure(plot_width=w, plot_height=h, x_axis_type = x_axis_type, title = title, **kwargs)
     p.line(x, y, color=colors[0])
     p.xgrid.visible = False
     p.ygrid.visible = False
